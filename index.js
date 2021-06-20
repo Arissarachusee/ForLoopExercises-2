0393dc8078d40296ece2eb3776650dda558cb149
ArissaraLorance <56275261+Arissarachusee@users.noreply.github.com>
2021-06-20 19:00:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,93 @@
+/*Exercise #1: Construct for loops that accomplish the following tasks:
+    a. Print the numbers 0 - 20, one number per line.
+    b. Print only the ODD values from 3 - 29, one number per line.
+    c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
+    d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
+
+
+for (let i= 0 ; i<=20 ; i++ ) {
+console.log(i);
+}
+
+console.log();
+
+for (let i= 3 ; i<29 ; i+=2 ) {
+console.log(i);
+}
+
+console.log();
+
+for (let i = 12 ; i >=-14; i-=2 ) {
+console.log(i);
+}
+
+console.log();
+
+for (let i = 50 ; i>=20 ; i-- ) {
+  if (i%3 === 0)
+console.log(i);
+}
+
+console.log();
+
+/*Exercise #2: 
+Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].*/
+
+  let word = "LaunchCode" ;
+  let mix = [1,5,'LC101','blue',42];
+
+/*Construct ``for`` loops to accomplish the following tasks:
+  a. Print each element of the array to a new line.*/
+
+for (let i = 0 ; i <= mix.length ; i++) {
+  console.log(mix[i]);
+  }
+
+console.log();
+/* b. Print each character of the string - in reverse order - to a new line. */
+
+for (let i = 0 ; i <= word.length ; i++) {
+let newWord = "";
+
+  console.log(word[i]);
+
+  }
+console.log();
+
+for (let i = word.length-1 ;i >=0 ; i--) {
+
+  console.log(word[i]);
+
+  }
+console.log();
+
+
+/*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
+  a. One array contains the even numbers, and the other holds the odds.
+  b. Print the arrays to confirm the results. */
+
+let array = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+
+let odd = [], even = [];
+
+for (i= 0 ; i < array.length ; i++ ) {
+   
+    if (array[i] %2 === 0){
+    even.push(array[i]);
+
+}else{
+       
+    odd.push(array[i]);
+
+}
+}
+console.log(odd);
+console.log(even);
+
+
+
+
+
+
+
+
